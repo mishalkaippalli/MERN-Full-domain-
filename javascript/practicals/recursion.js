0a5af00fe1eb33b491dab61197f35c562b000f88
Mishal <mishal9990@gmail.com>
2026-05-07 13:29:42 +0530

    mongodb pratcticals added

--- javascript/practicals/recursion.js
+++ javascript/practicals/recursion.js
@@ -13,3 +13,18 @@ function sumodd(arr){
    
 }
 console.log(sumodd(arr))
+
+//print fibonacci
+function fib(n){
+    
+    if(n==1) return [0];
+    if(n==2) return [0,1]
+    
+    let arr=fib(n-1)
+    
+    arr.push(arr[arr.length-1]+arr[arr.length-2])
+    
+    return arr
+}
+
+console.log(fib(4))
